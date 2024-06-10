3f6cbf2594ad02417618bc64a9092beb39b695aa
genesis0000 <genesis0000@bvm.network>
2024-06-10 15:43:16 +0700

    - update

--- assistant.go
+++ assistant.go
@@ -39,6 +39,7 @@ const (
 	AssistantToolTypeCodeInterpreter AssistantToolType = "code_interpreter"
 	AssistantToolTypeRetrieval       AssistantToolType = "retrieval"
 	AssistantToolTypeFunction        AssistantToolType = "function"
+	AssistantToolTypeFileSearch      AssistantToolType = "file_search"
 )
 
 type AssistantTool struct {
